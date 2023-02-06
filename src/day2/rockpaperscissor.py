85f6b0783e80a72cfc059a46577ac8f997e05db3
Pradeepta Swain <pswain@trifacta.com>
2023-02-06 22:34:22 +0530

    day2 part2

--- src/day2/rockpaperscissor.py
+++ src/day2/rockpaperscissor.py
@@ -1,11 +1,12 @@
 import os
 
 
-def main():
-    # Read the input data file, split the file by new line and store the values in a list
-    with open(os.path.join(os.getcwd(), "src/day2", "inputdata.txt"), "r") as f:
-        data = f.read()
+# Read the input data file, split the file by new line and store the values in a list
+with open(os.path.join(os.getcwd(), "src/day2", "inputdata.txt"), "r") as f:
+    data = f.read()
 
+
+def part1():
     total_score = 0
 
     # Move Rock gets a score of 1, Paper gets 2 and Scissor gets a score of 3
@@ -50,5 +51,49 @@ def main():
     return total_score
 
 
+def part2():
+    # rock > scissor > paper > rock
+    # Rock: [A, X] = 1
+    # Paper: [B, Y] = 2
+    # Scissor: [C, Z] = 3
+    # X -> Lose = 0
+    # Y -> Draw = 3
+    # Z -> Win = 6
+    total_score = 0
+    lose = 0
+    draw = 3
+    win = 6
+
+    move_x = 1
+    move_y = 2
+    move_z = 3
+
+    for item in data.split("\n"):
+        player_1_move = item.split(" ")[0]
+        final_outcome = item.split(" ")[1]
+
+        if player_1_move == "A" and final_outcome == "X":
+            total_score += lose + move_z
+        elif player_1_move == "A" and final_outcome == "Y":
+            total_score += draw + move_x
+        elif player_1_move == "A" and final_outcome == "Z":
+            total_score += win + move_y
+        if player_1_move == "B" and final_outcome == "X":
+            total_score += lose + move_x
+        elif player_1_move == "B" and final_outcome == "Y":
+            total_score += draw + move_y
+        elif player_1_move == "B" and final_outcome == "Z":
+            total_score += win + move_z
+        if player_1_move == "C" and final_outcome == "X":
+            total_score += lose + move_y
+        elif player_1_move == "C" and final_outcome == "Y":
+            total_score += draw + move_z
+        elif player_1_move == "C" and final_outcome == "Z":
+            total_score += win + move_x
+
+    return total_score
+
+
 if __name__ == "__main__":
-    print(f"Total Score: {main()}")
+    print(f"Total Score: {part1()}")
+    print(f"Total Score: {part2()}")
